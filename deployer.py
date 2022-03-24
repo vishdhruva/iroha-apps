9c4695596a40010b8bf92876859c42a6fba3bad4
Dhruva Vishwanath <vishwand@purdue.edu>
2022-03-24 15:49:43 -0400

    Removed sudo keyword for docker cmds

--- deployer.py
+++ deployer.py
@@ -18,9 +18,9 @@ def deployscript():
 
 #installation of docker network called tinychain
 
-        dockernetwork = '''sudo docker network create tinychain-iroha-network
+        dockernetwork = '''docker network create tinychain-iroha-network
 
-        sudo docker run --name some-postgres \
+        docker run --name some-postgres \
         -e POSTGRES_USER=postgres \
         -e POSTGRES_PASSWORD=mysecretpassword \
         -p 5432:5432 \
@@ -37,7 +37,7 @@ def deployscript():
 #cloning the git and installing docker
 
         gitclone = '''git clone -b develop https://github.com/hyperledger/iroha --depth=1
-        sudo docker run -it --name iroha -p 50051:50051 -v $(pwd)/iroha/example:/opt/iroha_data -v blockstore:/tmp/block_store --network=tinychain-iroha-network --entrypoint=/bin/bash hyperledger/iroha:x86_64-develop-latest
+        docker run -it --name iroha -p 50051:50051 -v $(pwd)/iroha/example:/opt/iroha_data -v blockstore:/tmp/block_store --network=tinychain-iroha-network --entrypoint=/bin/bash hyperledger/iroha:x86_64-develop-latest
         irohad --config config.docker --genesis_block genesis.block --keypair_name node0'''
 
         try:
